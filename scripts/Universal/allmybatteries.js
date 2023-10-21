c58ab8b55e5209b201253951ecc31849ad7802e3
komo1iddin <45395275+komo1iddin@users.noreply.github.com>
2023-10-21 10:15:32 +0500

    Update allmybatteries.js

--- scripts/Universal/allmybatteries.js
+++ scripts/Universal/allmybatteries.js
@@ -1,30 +1,3 @@
-/******************************
-
-脚本功能：电池管家——解锁订阅
-下载地址：https://is.gd/pSqvPm
-软件版本：1.8.3
-脚本作者：彭于晏💞
-更新时间：2023-10-5
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
-使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-
-*******************************
-
-[rewrite_local]
-
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/AllMyBatteries.js
-
-[mitm] 
-
-hostname = buy.itunes.apple.com
-
-
-*******************************/
-
-
 var ojbk = {
   "receipt": {
     "receipt_type": "Production",
