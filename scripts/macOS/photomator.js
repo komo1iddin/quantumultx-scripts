7439d2da7d79f1c81c1fd9ac2279eea85c9713cf
komo1iddin <45395275+komo1iddin@users.noreply.github.com>
2023-10-25 00:11:46 +0500

    create photomator script

---
+++ scripts/macOS/photomator.js
@@ -0,0 +1,29 @@
+var objc = JSON.parse($response.body);
+    objc = {
+        "subscriber": {
+          "entitlements": {
+            "pixelmator_photo_pro_access": {
+              "expires_date": "2099-10-24T17:51:11Z",
+              "product_identifier": "pixelmator_photo_yearly_v1",
+            }
+          },
+          "subscriptions": {
+            "pixelmator_photo_yearly_v1": {
+              "auto_resume_date": null,
+              "billing_issues_detected_at": null,
+              "expires_date": "2099-10-24T17:51:11Z",
+              "grace_period_expires_date": null,
+              "is_sandbox": false,
+              "original_purchase_date": "2023-10-17T17:51:12Z",
+              "ownership_type": "PURCHASED",
+              "period_type": "normal",
+              "purchase_date": "2023-10-17T17:51:11Z",
+              "refunded_at": null,
+              "store": "app_store",
+              "store_transaction_id": "90001753116771",
+              "unsubscribe_detected_at": null
+            }
+          }
+        }
+      }
+$done({body : JSON.stringify(objc)});
