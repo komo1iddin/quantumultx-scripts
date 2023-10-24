6f09c490ac209c91e113a26ccbb186b46bbfdee6
komo1iddin <45395275+komo1iddin@users.noreply.github.com>
2023-10-25 00:17:18 +0500

    Update photomator.js

--- scripts/macOS/photomator.js
+++ scripts/macOS/photomator.js
@@ -4,9 +4,12 @@ var objc = JSON.parse($response.body);
           "entitlements": {
             "pixelmator_photo_pro_access": {
               "expires_date": "2099-10-24T17:51:11Z",
+              "grace_period_expires_date": null,
               "product_identifier": "pixelmator_photo_yearly_v1",
+              "purchase_date": "2023-10-17T17:51:11Z"
             }
           },
+          "original_app_user_id": "$RCAnonymousID:8d7e08487c4a460b9f73f4de03424609",
           "subscriptions": {
             "pixelmator_photo_yearly_v1": {
               "auto_resume_date": null,
@@ -16,7 +19,6 @@ var objc = JSON.parse($response.body);
               "is_sandbox": false,
               "original_purchase_date": "2023-10-17T17:51:12Z",
               "ownership_type": "PURCHASED",
-              "period_type": "normal",
               "purchase_date": "2023-10-17T17:51:11Z",
               "refunded_at": null,
               "store": "app_store",
